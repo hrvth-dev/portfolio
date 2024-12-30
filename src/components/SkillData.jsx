ac1180256debb7d8b760caebc060ffb9fdb1a5b4
hrvth-dev <hrvth.dev@gmail.com>
2024-12-30 13:31:22 +0100

    Skill tömb object lérehozása.

---
+++ src/components/SkillData.jsx
@@ -0,0 +1,53 @@
+
+import reactImg from '../assets/reactjs.png';
+import angularImg from '../assets/angular.png';
+import gitImg from '../assets/git.png';
+import mongodbImg from '../assets/mongodb.png';
+import mysqlImg from '../assets/mysql.png';
+import tailwindCSSImg from '../assets/TailwindCSS.png';
+import javaScript from '../assets/js.png';
+import typescript from '../assets/typescript.png';
+
+export const skillData = [
+  {
+    id: 1,
+    name: "React",
+    image: reactImg
+  },
+  {
+    id: 2,
+    name: "Angular",
+    image: angularImg
+  },
+  {
+    id: 3,
+    name: "JavaScript",
+    image: javaScript
+  },
+  {
+    id: 4,
+    name: "TypeScript",
+    image: typescript
+  },
+
+  {
+    id: 5,
+    name: "Mysql",
+    image: mysqlImg
+  },
+  {
+    id: 6,
+    name: "MongoDB",
+    image: mongodbImg
+  },
+  {
+    id: 7,
+    name: "TailwindCSS",
+    image: tailwindCSSImg
+  },
+  {
+    id: 8,
+    name: "Git",
+    image: gitImg 
+  }
+];
