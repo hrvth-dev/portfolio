6289d15ff66a63e6011321bde05b6a6681459e01
hrvth-dev <hrvth.dev@gmail.com>
2024-12-30 13:35:41 +0100

    Komponensek hozzáaddása gyökér állományhoz.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import Header from "../src/components/Header.jsx";
+import AboutMe from './components/AboutMe.jsx';
+import Skills from './components/Skills.jsx';
 import "../src/App.css";
 
 function App() {
   return (
     <div className="App">
-      <Header />
-    
+      <Header/>
+      <AboutMe/>
+      <Skills/>
     </div>
   );
 }
